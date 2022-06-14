7efb8f071300312de57f7d403d51fa8086a4f8ff
anangbagus <anangbagus666@gmail.com>
2022-06-15 01:14:27 +0800

    [FIX] Rename Wishlist

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Whitelist extends Model {
+  class Wishlist extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       })
     }
   }
-  Whitelist.init({
+  Wishlist.init({
     product_id: DataTypes.INTEGER,
     user_id: DataTypes.INTEGER,
     createdBy: DataTypes.INTEGER,
@@ -31,5 +31,5 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Wishlist',
     tableName: 'wishlists',
   });
-  return Whitelist;
+  return Wishlist;
 };
