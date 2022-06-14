1ba28fb268d3a1d01955653f9721e2b4b32acab8
aribrilliantsyah <ariardiansyah101@gmail.com>
2022-06-14 15:08:37 +0700

    [ADD] Forgot Password

--- controllers/api/AuthController.js
+++ controllers/api/AuthController.js
@@ -119,7 +119,47 @@ class AuthController {
   }
   
   async forgot_password(req, res) {
+    let rules = {
+      email: 'required|email'
+    }
+
+    let validation = new Validator(req.body, rules);
+    if(validation.fails()){
+      return res.status(422).json({
+        status: false,
+        message: 'The form is not complete',
+        data: validation.errors.all()
+      })
+    }
+
+    let { email } = req.body
+    let user = await User.findOne({where: {email: email }})
+    if(!user?.email){
+      return res.status(200).json({
+        status: false,
+        message: 'Email not found'
+      })
+    }
+  
+    let otp = genOTP(6)
+    await User.update({otp: otp}, {where: {id: user.id}})
     
+    new Mailer({
+      from: process.env.MAIL_SENDER
+    }).prepare({
+      to: email,
+      subject: 'Forgot Password 🔒',
+      text: `OTP for reset password: ${otp}`,
+      html: `<h1>OTP for reset password: ${otp}</h1>`
+    }).send()
+
+    return res.status(200).json({
+      status: true,
+      message: 'Success OTP Send to Email',
+      data: {
+        email: email
+      }
+    })
   }
 
   async verify_otp(req, res){
