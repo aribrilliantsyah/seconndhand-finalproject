367b17a4765b3627d32aaad316c8a5f90103542b
aribrilliantsyah <ariardiansyah101@gmail.com>
2022-06-14 14:36:53 +0700

    [ADD] Login

--- controllers/api/AuthController.js
+++ controllers/api/AuthController.js
@@ -12,7 +12,48 @@ const Validator = require('validatorjs')
 class AuthController {
   
   async login(req, res) {
-    
+    let rules = {
+      email: 'required|email',
+      password: 'required' 
+    }
+
+    let validation = new Validator(req.body, rules);
+    if(validation.fails()){
+      return res.status(422).json({
+        status: false,
+        message: 'The form is not complete',
+        data: validation.errors.all()
+      })
+    }
+
+    let {email, password} = req.body
+
+    let user = await User.finOne({where: {email: email}})
+    if(!user?.email){
+      return res.status(200).json({
+        status: false,
+        message: 'Email not found'
+      })
+    }
+
+    if(!bcrypt.compareSync(password, user?.password)){
+      return res.status(200).json({
+        status: false,
+        message: 'Invalid password'
+      })
+    }
+
+    let token = jwt.sign({id: user_game?.id, email: email}, privateKey, { expiresIn: '1d'})
+    await User.update({token: token}, {where: {id: user.id}})
+
+    return res.status(200).json({
+      status: true,
+      message: 'Email & Password Match',
+      data: {
+        token: token,
+        expired_at: moment().add(1, 'days').format('YYYY-MM-DD HH:mm:ss')
+      }
+    })
   }
 
   async register(req, res) {
