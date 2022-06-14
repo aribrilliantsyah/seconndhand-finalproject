2aabf53e68916f137d98582f6eaf31bba1bb4c8e
aribrilliantsyah <ariardiansyah101@gmail.com>
2022-06-14 17:25:29 +0700

    [ADD] Setup JWT

--- middleware/Middleware.js
+++ middleware/Middleware.js
@@ -3,44 +3,36 @@ const privateKey = process.env.JWT_PRIVATE_KEY
 
 class Middleware {
 
-    static verifyJwt(req, res, next){
-        const authHeader = req?.headers['authorization']
-        if(!authHeader){
-            return res.status(401).json({
-                'message': 'Unauthorized'
-            })
-        }
-
-        const token = authHeader && authHeader.split(' ')[1]
-
-        if(authHeader.split(' ')[0] != 'Bearer') return res.status(422).json({
-            'message': 'Invalid Bearer Token'
-        })
-
-        if (token == null) return res.status(401).json({
-            'message': 'Unauthorized'
-        })
-
-        jwt.verify(token, privateKey, (err, user) => {
-            // console.log(user)
-            if (err) return res.status(403).json({
-                'message': 'Forbidden'
-            })
-
-            req.user = user
-            next()
-        })
+  static verifyJwt(req, res, next){
+    const authHeader = req?.headers['authorization']
+    if(!authHeader){
+      return res.status(401).json({
+        status: false,
+        message: 'Unauthorized'
+      })
     }
 
-    static verifyJwtPage(req, res, next){
-        let token = req?.session?.token
+    const token = authHeader && authHeader.split(' ')[1]
+    if(authHeader.split(' ')[0] != 'Bearer') return res.status(422).json({
+      status: false,
+      message: 'Invalid Bearer Token'
+    })
 
-        jwt.verify(token, privateKey, (err, user) => {
-            if (err) res.redirect('/login');
-            req.user = user
-            next()
-        })
-    }
+    if (token == null) return res.status(401).json({
+      status: false,
+      message: 'Unauthorized'
+    })
+
+    jwt.verify(token, privateKey, (err, user) => {
+      if (err) return res.status(403).json({
+        message: 'Forbidden'
+      })
+
+      req.user = user
+      next()
+    })
+  }
+  
 }
 
 module.exports = Middleware
