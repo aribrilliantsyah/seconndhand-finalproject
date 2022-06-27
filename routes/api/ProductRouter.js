f11b825273653ef8011e73a98576d7ae060433aa
aribrilliantsyah <ariardiansyah101@gmail.com>
2022-06-27 16:57:52 +0700

    [FIX] Product get by id public

--- routes/api/ProductRouter.js
+++ routes/api/ProductRouter.js
@@ -31,7 +31,7 @@ const upload = multer({
 
 router.get(`/product/public`, ctl.getAll)
 router.get(`/product/`, Middleware.verifyJwt, Middleware.checkProfile, ctl.getAll)
-router.get(`/product/public/:id`, ctl.findByID)
+router.get(`/product/:id/public`, ctl.findByID)
 router.get(`/product/:id`, Middleware.verifyJwt, Middleware.checkProfile, ctl.findByID)
 router.post(`/product`, Middleware.verifyJwt, Middleware.checkProfile, ctl.create)
 router.put(`/product/:id`, Middleware.verifyJwt, Middleware.checkProfile, ctl.update)
